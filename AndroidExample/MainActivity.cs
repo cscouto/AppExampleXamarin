652c4c5cbd972b5f75e025a2aa4cd686eebfdae3
TIAGO-LAPTOP\Tiago <cs.couto@outlook.com>
2017-06-15 19:16:10 -0300

    final

---
+++ AndroidExample/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidExample
+{
+    [Activity(Label = "AndroidExample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //Define o conteudo da view
+            SetContentView(Resource.Layout.Main);
+
+            // cria um botao
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            //coloca um evento no click
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
---
+++ WinPhoneExample/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WinPhoneExample.Resources;
+
+namespace WinPhoneExample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        int count = 1;
+        // Constructor
+        public MainPage()
+        {
+            
+            InitializeComponent();
+
+        }
+
+        private void btHello_Click(object sender, RoutedEventArgs e)
+        {
+            btHello.Content = string.Format("{0} clicks!", count++);
+        }
+
+    }
+}
